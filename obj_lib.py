6c8a19f820d820a134365dfd7b839c5f15891f43
dabadee <74227807+hassaan253@users.noreply.github.com>
2022-06-20 16:40:20 +0500

    further material file manipulation
    can change dif variables, combine multiple material files and save them into a new one

--- obj_lib.py
+++ obj_lib.py
@@ -16,11 +16,9 @@ class WavefrontOBJ:
         self.polygons  = []                 # M*Nv*3 array, Nv=# of vertices, stored as vid,tid,nid (-1 for N/A)
         self.faces     = []                 #stores all faces
         
-    def load_obj(self, filename: str , max_face, default_mtl='default_mtl', triangulate=False) -> WavefrontOBJ:
+def load_obj( filename: str , max_face, default_mtl='default_mtl', triangulate=False) -> WavefrontOBJ:
     """Reads a .obj file from disk and returns a WavefrontOBJ instance
-
     Handles only very rudimentary reading and contains no error handling!
-
     Does not handle:
         - relative indexing
         - subobjects or groups
@@ -36,37 +34,20 @@ class WavefrontOBJ:
         nid = int(vals[2]) if len(vals) > 2 else 0
         return (vid,tid,nid)
     """
- ## this function will take the individual 3d coordinate for the vertex
-    #and rotate it and translate it according to input
-    def rotate_obj( s, angle_x = 0, angle_y = 0,angle_z= 0, trans_x = 0, trans_y = 0, trans_z = 0 ):
-                #angle_x,angle_y,angle_z are inputs 
-        #which tell amount of rotation in each specific axis
-        #then we multiply our vertcies with the transformation matrix
-        # for rotation in each matrix
-        v1=s[0].split("//") # x-cord
-        v2=s[1].split("//") # y- cord
-        v3=s[2].split("//") # z- cord
-     # translation by adding value to each vertex
-        v1=int(v1[0])
-        v1 += trans_x
+ 
+    def parse_vector( s ):
+        v1=s[0].split("//")
+        v2=s[1].split("//")
+        v3=s[2].split("//")
+        
+        v1=int(v1[0])    
         v2=int(v2[0])
-        v2 += trans_y
         v3=int(v3[0])
-        v3+= trans_z
+        
         list1=[v1,v2,v3]
-        #rotation by multiplying each 3x1 vertex vector by rotation matrix
-        # using np to calculate sin,cos
-        #angle input in degrees
-        r_x =[[1,0,0],[0,np.cos(angle_x),-np.sin(angle_x)], [0,np.sin(angle_x),np.cos(angle_x)]]
-        r_y = [[np.cos(angle_y),0,np.sin(angle_y)],[0,1,0], [-np.sin(angle_y),0,np.cos(angle_y)]]
-        r_z = [[np.cos(angle_z),-np.sin(angle_z),0],[np.sin(angle_z),np.cos(angle_z),0], [0,0,1]]
-        # the list1 is being updated as it is being dotted by the 3 matrices
-        list1 = np.dot(list1, r_x)
-        list1 = np.dot(list1, r_y)
-        list1 = np.dot(list1, r_z)
         return list1
-    # complete this so we know what to do with the saved list of vertices
-    #list 1 is basically the 3d verter points after being rotated 
+    
+
 
     with open( filename, 'r' ) as objf:
         
@@ -87,13 +68,7 @@ class WavefrontOBJ:
             elif toks[0] == 'f':
                 #obj.faces.append( [ float(v) for v in toks[1:]] )
                 #poly = [ parse_vertex(vstr) for vstr in toks[1:] ]
-                #a = [parse_vector(toks[1:])]
-                #--------------------------------------
-                #instead of calling the parse vector, we are calling the rotate object function
-                #the object has 6 additional parameters
-                # angles for rotation along x,y,z axis
-                # and value for translation x , y , or z direction
-                a = [rotate_obj(toks[1:], 45,45,45, 10,10,-5)]
+                a = [parse_vector(toks[1:])]
                 
                 poly=a[0]
               
@@ -124,11 +99,9 @@ class WavefrontOBJ:
         print(max_face)
         return obj
 
-    def save_obj( obj: WavefrontOBJ, filename: str ):
+def save_obj( obj: WavefrontOBJ, filename: str ):
     """Saves a WavefrontOBJ object to a file
-
     Warning: Contains no error checking!
-
     """
     with open( filename, 'a' ) as ofile:
         ofile.write("o B"+'\n')
@@ -163,24 +136,56 @@ class WavefrontOBJ:
                 pstr += vstr
             ofile.write( pstr+'\n')
         """
+# usemtl 
+#################################################################################################################
+
+def rotate_obj( obj: WavefrontOBJ, angle_x = 0, angle_y = 0,angle_z= 0):
+    for vtx in range(len(obj.vertices)):
+        list1= obj.vertices[vtx]
+            #rotation by multiplying each 3x1 vertex vector by rotation matrix
+            # using np to calculate sin,cos
+            #angle input in degrees v = [1,2,3]
+        r_x =[[1,0,0],[0,np.cos(angle_x),-np.sin(angle_x)], [0,np.sin(angle_x),np.cos(angle_x)]]
+        r_y = [[np.cos(angle_y),0,np.sin(angle_y)],[0,1,0], [-np.sin(angle_y),0,np.cos(angle_y)]]
+        r_z = [[np.cos(angle_z),-np.sin(angle_z),0],[np.sin(angle_z),np.cos(angle_z),0], [0,0,1]]
+            # the list1 is being updated as it is being dotted by the 3 matrices
+        list1 = np.dot(list1, r_x)
+        list1 = np.dot(list1, r_y)
+        list1 = np.dot(list1, r_z)
+        obj.vertices[vtx] = list1
+def translate_obj(obj: WavefrontOBJ, trans_x = 0, trans_y = 0, trans_z = 0 ):
+      for vtx in range(len(obj.vertices)):
+        v1= obj.vertices[vtx][0]
+        v1 += trans_x
+        v2= obj.vertices[vtx][1]
+        v2 += trans_y
+        v3= obj.vertices[vtx][2]
+        v3+= trans_z
+        list1=[v1,v2,v3]
+        obj.vertices[vtx] = list1
+    ##############
+
+
 
 
 # CLASS FOR MATERIAL OBJECT FILES
+
+
 class mtlobj:
     def __init__(self):
         self.path = None
         #the material file has the following values
-        self.name = []
-        self.Ka = [] #s a 3 integer value ex. 3.000 2.000 1.000
-        self.Ks = [] #all values that start with K have 3 float values
-        self.Kd = []
-        self.Ke = []
-        self.illum = [] #a single float
-        self.Ns = [] #a single float
-        self.d = [] #a single float
-        self.Ni = [] #a single float
-        self.map_Kd = [] #file path to image for texture
-
+        self.name = [""]
+        self.Ka = [["0","0","0"]] #s a 3 integer value ex. 3.000 2.000 1.000
+        self.Ks = [["0","0","0"]] #all values that start with K have 3 float values
+        self.Kd = [["0","0","0"]]
+        self.Ke = [["0","0","0"]]
+        self.illum = ["0","0"] #a single float
+        self.Ns = ["0","0"] #a single float
+        self.d = ["0","0"] #a single float
+        self.Ni = ["0","0"] #a single float
+        self.map_Kd = [""] #file path to image for texture
+###
     def load_mtl(self, filename: str):
         #file is being read and each value being stored
         with open( filename, 'r' ) as mtl:
@@ -188,68 +193,102 @@ class mtlobj:
             for line in range(len(lines)):
                 lines[line].strip()
                 lines[line].strip("\n")
-                ## right now values are being stored as strings,
-                #if we want to manipulate, will need to convert to floats
-                 if lines[line][0] == "n" and lines[line][1] == "e":
+                if lines[line][0] == "n" and lines[line][1] == "e":
                     self.name.append(lines[line][:-1])
-                if lines[line][0] == 'N' and lines[line][1] == 's':
+                elif lines[line][0] == 'N' and lines[line][1] == 's':
                     val = lines[line][3:-1]
                     self.Ns.append(val)
-                if lines[line][0] == "N" and lines[line][1] == "i":
+                elif lines[line][0] == "N" and lines[line][1] == "i":
                     self.Ni.append([lines[line][3:-1]])
-                if lines[line][0] == "d":
+                elif lines[line][0] == "d":
                     self.d.append([lines[line][2:-1]])
-                if lines[line][0] == "K" and lines[line][1] == "a":
+                elif lines[line][0] == "K" and lines[line][1] == "a":
                     self.Ka.append([lines[line][3:-1]])
-                if lines[line][0] == "K" and lines[line][1] == "s":
+                elif lines[line][0] == "K" and lines[line][1] == "s":
                     self.Ks.append([lines[line][3:-1]])
-                if lines[line][0] == "K" and lines[line][1] == "e":
+                elif lines[line][0] == "K" and lines[line][1] == "e":
                     self.Ke.append([lines[line][3:-1]])
-                if lines[line][0] == "K" and lines[line][1] == "d":
+                elif lines[line][0] == "K" and lines[line][1] == "d":
                     self.Kd.append([lines[line][3:-1]])
-                if lines[line][0] == "m" and lines[line][1] == "a":
+                elif lines[line][0] == "m" and lines[line][1] == "a":
                     self.map_Kd.append([lines[line][7:-1]])
-                if lines[line][0] == "i":
+                elif lines[line][0] == "i":
                     self.illum.append([lines[line][6:-1]])
-
+                else:
+                    print("Unidentified Programming Object")
+ 
     def save_mtl(self, name: str):
-    """Saves values in material file to new file
-    Warning: Contains no error checking
-    """
-    # store the values in a string in the right order, and write it to the file
-    #TO-Do, add the labels within the string such as Ka, Ks etc
-    filename = name
-    with open( filename, 'a' ) as ofile:
-        for i in range len(self.name):
-            material =  "\n" + str(self.name[i] + "\n" self.Ns[i] + "\n" + self.Ka[i] + "\n" self.Kd[i] + "\n" + self.Ks[i] + "\n" + self.Ke[i] + "\n"  + self.Ni[i]+ "\n" + self.d[i] + "\n" + self.illum[i]+ "\n" +self.map_Kd[i] + "\n"
-            ofile.write(material)
-        #helper functions, allow user to change value of texture parameters
-        # have to enter the number in array of material file which they want to access 
+        """Saves values in material file to new file
+        Warning: Contains no error checking
+        """
+        filename = name
+        # store the values in a string in the right order, and write it to the file
+        #TO-Do, add the labels within the string such as Ka, Ks etc
+        with open( filename, 'w' ) as ofile:
+            for i in range(len(self.name)): 
+                material =  ("\n" + "newmtl " + 
+                             str(self.name[i]) + "\n"+
+                             "Ns " + str(self.Ns[i]) + "\n" + 
+                             "Ka " + str(self.Ka[i][0])+" " + str(self.Ka[i][1])+" " + str(self.Ka[i][2])+"\n"+
+                             "Kd " + str(self.Kd[i][0])+" " + str(self.Kd[i][1])+" " + str(self.Kd[i][2])+"\n"+
+                             "Ks " + str(self.Ks[i][0])+" " + str(self.Ks[i][1])+" " + str(self.Ks[i][2])+"\n"+ 
+                             "Ke "+str(self.Ke[i][0])+" " + str(self.Ke[i][1])+" " + str(self.Ke[i][2])+"\n"+
+                             "Ni "+str(self.Ni[i])+ "\n" + 
+                             "d "+str(self.d[i]) + "\n" + 
+                             "illum "+str(self.illum[i]) +"\n" + 
+                             "map_Kd C:\\\\Users\\\\Hassaan\\\\Documents\\\\" + str(self.map_Kd[i]))
+                ofile.write(material)
+            #helper functions, allow user to change value of texture parameters
+            # have to enter the number in array of material file which they want to access 
     def ambient_clr(self, num: int, r:int,g:int,b:int):
-        self.Ka[num][0] = r
-        self.Ka[num][1] = r
-        self.Ka[num][2] = r
+        if(num >= len(self.Ka)):
+            self.Ka.append([r,g,b])
+        else:
+            self.Ka[num] = [r,g,b]
         
     def diffuse_clr(self, num: int, r:int,g:int,b:int):
-        self.Kd[num][0] = r
-        self.Kd[num][1] = r
-        self.Kd[num][2] = r
+        if(num >= len(self.Kd)):
+            self.Kd.append([r,g,b])
+        else:
+            self.Kd[num] = [r,g,b]
         
     def spec_clr(self, num: int, r:int,g:int,b:int):
-        self.Ks[num][0] = r
-        self.Ks[num][1] = r
-        self.Ks[num][2] = r
-
-    def illum(self, num:int, illumination: int):
-        self.illum[num] = illumination
+        if(num >= len(self.Ks)):
+            self.Ks.append([r,g,b])
+        else:
+            self.Ks[num] = [r,g,b]
+    
+    def setKe(self, num: int, r:int,g:int,b:int):
+        if(num >= len(self.Ke)):
+            self.Ke.append([r,g,b])
+        else:
+            self.Ke[num] = [r,g,b]
+            
+    def illumi(self, num, illumination):
+        if(num >= len(self.illum)):
+            self.illum.append(illumination)
+        else:
+            self.illum[num] = str(illumination)
     def shine(self, num:int, shine: int):
-        self.Ns[num] = shine
+        if(num >= len(self.Ns)):
+            self.Ns.append(shine)
+        else:
+            self.Ns[num] = str(shine)
 
     def transparency(self, num:int, Tr: int):
-        self.d[num] = Tr
+        if(num >= len(self.d)):
+            self.d.append(Tr)
+        else:
+            self.d[num] = str(Tr)
 
     def texture(self, path: str, num: int):
-        self.map_Ka[num] = path
-        
-
+        if(num >= len(self.map_Kd)):
+            self.map_Kd.append(path)
+        else:
+            self.map_Kd[num] = str(path)
+    def set_name(self, num: int, path: str):
+        if(num >= len(self.name)):
+            self.name.append(path)
+        else:
+            self.name[num] = str(path)
         
